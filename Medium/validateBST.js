73490684593bd4adfdb13b39707505edcf455943
Your Name <you@example.com>
2019-03-28 18:21:59 -0700

    Complete ValidateBST

---
+++ Medium/validateBST.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+const isValidBST = root => validator(root);
+
+const validator = (root, min = null, max = null) => {
+  if (root === null) {
+    return true;
+  }
+  if (min !== null && root.val <= min) {
+    return false;
+  }
+  if (max !== null && root.val >= max) {
+    return false;
+  }
+
+  return (
+    validator(root.left, min, root.val) && validator(root.right, root.val, max)
+  );
+};
