d0831a4c9192dc25a43e01e30066cc1c4c80e8bf
Your Name <you@example.com>
2019-03-29 22:09:34 -0700

    Complete rotateMatrix

---
+++ Easy/rotateMatrix.js
@@ -0,0 +1,17 @@
+//Given an image represented by an NxN matrix, rotate the image by 90 degrees
+
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const rotateMatrix = matrix => {
+  let output = [];
+  for (let i = matrix[0].length - 1; 0 <= i; i--) {
+    let trio = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      trio.unshift(matrix[j][i]);
+    }
+    output.unshift(trio);
+  }
+  return output;
+};
+
+rotateMatrix(matrix);
