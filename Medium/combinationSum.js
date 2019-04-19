df62e0379f2c1f86f2548ccbf55ab4b8bd622f00
Your Name <you@example.com>
2019-04-19 14:52:22 -0700

    Complete combinationSum

---
+++ Medium/combinationSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum = (candidates, target) => {
+  const result = [];
+
+  const permutate = (candidates, target, index, permutations = []) => {
+    if (target === 0) {
+      result.push(permutations);
+    }
+
+    for (let i = index; i < candidates.length; i++) {
+      if (target - candidates[i] >= 0) {
+        permutate(
+          candidates,
+          target - candidates[i],
+          i,
+          permutations.concat(candidates[i])
+        );
+      }
+    }
+  };
+  permutate(candidates, target, 0, []);
+  return result;
+};
