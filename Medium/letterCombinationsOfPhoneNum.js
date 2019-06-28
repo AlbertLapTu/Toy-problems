eb7c9ab87da99fad08d4a6f70a403ad23135aeea
Your Name <itzalbert2@gmail.com>
2019-06-27 17:53:09 -0700

    17. Letter Combinations of Phone

---
+++ Medium/letterCombinationsOfPhoneNum.js
@@ -0,0 +1,31 @@
+var letterCombinations = function(digits) {
+  if (digits.length === 0) return [];
+
+  const phoneMap = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+  };
+
+  const permutations = [];
+
+  const permutate = (str, permutation) => {
+    if (str.length === 0) {
+      permutations.push(permutation);
+    } else {
+      let digits = phoneMap[str[0]];
+
+      for (char of digits) {
+        permutate(str.substring(1), permutation.concat(char));
+      }
+    }
+  };
+
+  permutate(digits, '');
+  return permutations;
+};
