7f3c8dcfbb4ad67653905582096d1eaca16edf72
Your Name <itzalbert2@gmail.com>
2019-12-18 10:20:42 -0800

    modify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['eslint:recommended', 'airbnb'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always']
-  }
+    semi: ['error', 'always'],
+    'multiline-ternary': ['error', 'always'],
+  },
 };
