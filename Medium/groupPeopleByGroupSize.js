0bee2eb688d7bfef1a9fb5dc6fe1da96478982ea
Albert <itzalbert2@gmail.com>
2019-12-18 20:40:42 -0800

    Group People by Group size linting

--- Medium/groupPeopleByGroupSize.js
+++ Medium/groupPeopleByGroupSize.js
@@ -1,10 +1,11 @@
 /*
 
-There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group. Given the array groupSizes of length n telling the group size each person belongs to, return the groups there are and the people's IDs each group includes.
+There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group. 
+Given the array groupSizes of length n telling the group size each person belongs to, return the 
+groups there are and the people's IDs each group includes.
 
-You can return any solution in any order and the same applies for IDs. Also, it is guaranteed that there exists at least one solution. 
-
- 
+You can return any solution in any order and the same applies for IDs. Also, it is guaranteed that 
+there exists at least one solution. 
 
 Example 1:
 
@@ -21,7 +22,6 @@ Output: [[1],[0,5],[2,3,4]]
 
 function groupThePeople(groupSizes) {
   const hash = group(groupSizes);
-  const res = [];
 
   for (let key in hash) {
     if (hash[key].length > key) {
