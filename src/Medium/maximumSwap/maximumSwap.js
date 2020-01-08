e4cbc11c166ab6d9fa57f1a396c126d159dd5592
Albert <itzalbert2@gmail.com>
2020-01-08 11:14:27 -0800

    670. Maximum Swap

--- src/Medium/maximumSwap/maximumSwap.js
+++ src/Medium/maximumSwap/maximumSwap.js
@@ -1,64 +1,58 @@
 /*
 
-Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. 
-Return the maximum valued number you could get.
+Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the maximum valued number you could get.
 
 Example 1:
-Input: 2736
-Output: 7236
-Explanation: Swap the number 2 and the number 7.
+  Input: 2736
+  Output: 7236
+  Explanation: Swap the number 2 and the number 7.
 
-Analysis:
+Example 2:
+  Input: 9973
+  Output: 9973
+  Explanation: No swap.
 
-The problem calls for some type of iteration and swap, and given the input is an integer, some
-housekeeping needs to be done with the input to let us work with it. 
-
-Swapping values infers we need to keep track of the indices based off of the conditions that are set. 
-
-Iterate over the number
-  For the current number
-    Assign max number that's one more than our current
-    Keep track of indices
-    Iterate over the rest of the numbers
-      if the number[j] is greater than max
-        Update max to number[j]
-        Update second index
-    
-    If we found the index where we should swap, break;
-
-
-Time complexity: (O^2)
-Space: O(1). Maximum value input received will be 10^8, therefore mem. allocation of array length 10.
 */
 
-const maximumSwap = (num) => {
+/**
+ * @param {number} num
+ * @return {number}
+ */
+
+var maximumSwap = function(num) {
   if (num === 0) return num;
-  const numArr = num.toString().split('');
+  let numArr = num.toString().split('');
   let max, idx1, idx2;
 
   for (let i = 0; i < numArr.length; i++) {
-    let current = Number(numArr[i]);
-    max = current + 1;
+    let curr = parseInt(numArr[i]);
+    // We want to find the first biggest number that doesn't equal the current value
+    max = curr + 1;
+
+    // If there are no numbers after the current assumed max,
+    // idx1 and idx2 will be the same.
     idx1 = i;
-    idx2 = i; //We need to keep track of indices in case there is a tie in maximums.
+    idx2 = i;
+
     for (let j = i + 1; j < numArr.length; j++) {
-      let next = Number(numArr[j]);
+      let next = parseInt(numArr[j]);
+      // If we find a max, log its position and update max
       if (next >= max) {
-        max = next;
         idx2 = j;
+        max = next;
       }
     }
+
+    // After first swap, we should have largest number
     if (idx1 !== idx2) break;
   }
 
   swap(numArr, idx1, idx2);
-  return Number(numArr.join(''));
+  return parseInt(numArr.join(''));
 };
 
-const swap = (arr, i, j) => {
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-};
-
-console.log(maximumSwap(2736)); //7236
-console.log(maximumSwap(3361)); //6331
-console.log(maximumSwap(9973)); //9973
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
