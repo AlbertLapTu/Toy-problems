e7db9fccbb2d6936b1b0147214b4617f3f1b5f49
Albert <itzalbert2@gmail.com>
2020-01-09 12:32:36 -0800

    64. Minimum Path Sum

--- src/Medium/minimumPathSum/__tests__/minimumPathSum.test.js
+++ src/Medium/minimumPathSum/__tests__/minimumPathSum.test.js
@@ -10,12 +10,12 @@ describe('64. Minimum Path Sum', () => {
       [1, 3, 1, 5],
       [1, 5, 1, 6],
       [4, 2, 1, 10],
-    ]; // 17
+    ];
     const testCase2 = [
       [1, 2],
       [3, 4],
       [5, 6],
-    ]; //13
+    ];
 
     expect(fn(testCase1)).toBe(17);
     expect(fn(testCase2)).toBe(13);
