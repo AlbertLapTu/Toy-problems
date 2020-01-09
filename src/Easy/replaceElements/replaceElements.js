88956014e49ddb9786b027e2dabbca634e2573da
Albert <itzalbert2@gmail.com>
2020-01-09 09:13:22 -0800

    1299. Replace Elements with Greatest Element on right side

---
+++ src/Easy/replaceElements/replaceElements.js
@@ -0,0 +1,36 @@
+/*
+
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+
+ 
+
+Example 1:
+
+Input: arr = [17,18,5,4,6,1]
+Output: [18,6,6,6,1,-1]
+
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+
+var replaceElements = function(arr) {
+  if (!arr.length) return arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    let greatest = 0;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      greatest = Math.max(greatest, arr[j]);
+    }
+
+    arr[i] = greatest;
+  }
+
+  arr[arr.length - 1] = -1;
+  return arr;
+};
