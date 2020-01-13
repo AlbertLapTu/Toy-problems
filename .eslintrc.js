4d6eefe23485e07c70ed0f68e78d369a7d03eef5
Albert <itzalbert2@gmail.com>
2020-01-13 09:16:16 -0800

    chore(lint): Strip out airbnb linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'airbnb'],
+  extends: ['eslint:recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
