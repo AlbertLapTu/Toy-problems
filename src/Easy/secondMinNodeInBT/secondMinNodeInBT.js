0be218b17fe37825ab40262271db0b0c50eb4d02
Albert <itzalbert2@gmail.com>
2020-01-16 09:19:01 -0800

    671. Second solution

--- src/Easy/secondMinNodeInBT/secondMinNodeInBT.js
+++ src/Easy/secondMinNodeInBT/secondMinNodeInBT.js
@@ -39,37 +39,63 @@ Explanation: The smallest value is 2, but there isn't any second smallest value.
  * @param {TreeNode} root
  * @return {number}
  */
+
+// Time O(n) traversal + O(n log n) sort + O(n) last iteration
+// 2O(n) + O(n log n) :(
+// function secondMinNodeInBT(root) {
+//   let output = [];
+
+//   function traverse(node) {
+//     if (!node) return null;
+//     if (!node.left && !node.right) {
+//       output.push(node.val);
+//       return;
+//     }
+
+//     let min = Math.min(node.left.val, node.right.val);
+//     output.push(min);
+//     traverse(node.left);
+//     traverse(node.right);
+//   }
+
+//   traverse(root);
+//   // Now you have an array of values, where you need to find the second highest
+//   let min = Math.min(...output);
+//   output = output.sort((a, b) => a - b);
+//   let nextMin = min;
+
+//   for (let i = 0; i < output.length; i++) {
+//     let curr = output[i];
+//     if (curr > min) {
+//       nextMin = curr;
+//       break;
+//     }
+//   }
+
+//   return nextMin === min ? -1 : nextMin;
+// }
+
+// Better solution O(n);
 function secondMinNodeInBT(root) {
-  let output = [];
+  let min = root.val;
+  let res = Number.MAX_SAFE_INTEGER;
 
-  function traverse(node) {
-    if (!node) return null;
-    if (!node.left && !node.right) {
-      output.push(node.val);
-      return;
-    }
+  function dfs(node) {
+    if (!node) return;
 
-    let min = Math.min(node.left.val, node.right.val);
-    output.push(min);
-    traverse(node.left);
-    traverse(node.right);
-  }
-
-  traverse(root);
-  // Now you have an array of values, where you need to find the second highest
-  let min = Math.min(...output);
-  output = output.sort((a, b) => a - b);
-  let nextMin = min;
-
-  for (let i = 0; i < output.length; i++) {
-    let curr = output[i];
-    if (curr > min) {
-      nextMin = curr;
-      break;
+    // If we havent found the min yet, keep traversing
+    if (node.val === min) {
+      dfs(node.left);
+      dfs(node.right);
+      // If we found the next greatest value
+    } else if (node.val > min && node.val < res) {
+      res = node.val;
     }
   }
 
-  return nextMin === min ? -1 : nextMin;
+  dfs(root);
+  // If we havent found the next greater value, res will stay the same
+  return res === Number.MAX_SAFE_INTEGER ? -1 : res;
 }
 
 module.exports = secondMinNodeInBT;
