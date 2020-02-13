46c562e1cc5a6868662f19e2fd90864d7a813e8f
Your Name <itzalbert2@gmail.com>
2020-02-13 11:59:25 -0800

    1013. Remove comments

--- src/Easy/canThreePartsEqualSum/__tests__/canThreePartsEqualSum.test.js
+++ src/Easy/canThreePartsEqualSum/__tests__/canThreePartsEqualSum.test.js
@@ -6,9 +6,9 @@ describe('Easy - Partition array into equal parts sum', () => {
   });
 
   test('It should return a boolean if the array can be partitioned', () => {
-    const input1 = [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1]; // true
-    const input2 = [0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1]; // false
-    const input3 = [3, 3, 6, 5, -2, 2, 5, 1, -9, 4]; // true
+    const input1 = [0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1];
+    const input2 = [0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1];
+    const input3 = [3, 3, 6, 5, -2, 2, 5, 1, -9, 4];
 
     expect(fn(input1)).toBeTruthy();
     expect(fn(input2)).toBe(false);
