951a67885ac266e475dc86de7c0d7606b20ac7d1
Albert <itzalbert2@gmail.com>
2020-05-10 17:54:23 -0700

    1431. Add tests

---
+++ src/Easy/kidsWithMostCandies/__tests__/kidsWithMostCandies.test.js
@@ -0,0 +1,7 @@
+const fn = require('../kidsWithMostCandies');
+
+describe('1431. Kids With the Greatest Number of Candies', () => {
+  test('It should exist', () => {
+    expect(fn).toBeTruthy();
+  });
+});
